4f83d019b68e0d17ba572c136a19d2675841d9fe
trestletech <jeff.allen@trestletechnology.net>
2014-09-25 00:07:42 -0500

    Did some basic analysis in Rmd.

--- LakeCounty/demographics.R
+++ LakeCounty/demographics.R
@@ -23,3 +23,19 @@ ages <- assignAges(ages, 70, 74, 4.3)
 ages <- assignAges(ages, 75, 79, 3.1)
 ages <- assignAges(ages, 80, 84, 2.3)
 ages <- assignAges(ages, 85, 90, 2.1)
+
+# I'm guessing here, since i don't have a key.
+raceAbbrevs <- list(I="Indian", W="White", H="Hispanic", P="Pacific", B="Black", O="Other")
+# There are others, which should just be classified as Other
+
+# From wikipedia, as of 2011
+# The percentages on Wikipedia add up to 116.6%, so I guess people can be counted twice.
+# We'll just normalize over 1.166 -- it should get us close to some simplified version of reality.
+# Create a named array
+races <- unlist(list(I=.032, W=.846, H=.167, P=.011, B=.022, O=.088))
+races <- races/1.166
+
+
+# http://quickfacts.census.gov/qfd/states/06/06033.html Says 50% female
+gender <- list(M=.5, F=.5, O=0.0)
+genAbbrev <- list(M="Male", F="Female", O="Other")
--- LakeCounty/parseRecords.R
+++ LakeCounty/parseRecords.R
@@ -6,18 +6,28 @@ library(lubridate)
 downloadDate <- function(month, day, year){  
   dat <- GET("http://acm.co.lake.ca.us/sheriff/arrests.asp", query=list(ArrestDate=paste(month, day, year, sep="/")))
   if (dat$headers$status != "200"){
-    stop ("Non-200 status returned from GET request to server.")
+    # e.g. 12/03/2013 gives a 500 with an error on the page, but works just fine.
+    warning ("Non-200 status returned from GET request to server:", month, "-", day, "-", year)
   }  
   con <- content(dat, as="text")  
   html <- htmlParse(con, asText=TRUE)
   tab <- readHTMLTable(html)
-  if (length(tab) == 2){
+  if (length(tab) <= 2){
     tab <- tab[[1]]
   }
   legit <- which(grepl("^[[:digit:]]+$", as.character(tab[,1])))
   colnames(tab) <- as.character(unlist((tab[min(legit)-1,])))
   tab <- tab[legit,]
   tab
+}
+
+# Worried about putting too many pictures into a single dir. Split them up into 100
+# directories by their last two digits.
+# Also creates the dir if it doesn't exist.
+getImagePath <- function(id){
+  dir <- substr(id, nchar(id)-1, nchar(id))
+  dir.create(paste0("data/pics/", dir), showWarnings = FALSE, recursive = TRUE)
+  paste0("data/pics/",dir,"/",id,".jpg")
 }
 
 #' Download the details of the particular booking ID provided.
@@ -30,19 +40,39 @@ downloadBooking <- function(bookingID){
   con <- content(dat, as="text")
   html <- htmlParse(con, asText=TRUE)
   
+  # Get the mugshot
+  img <- xmlGetAttr(getNodeSet(html, path = "//img")[[1]], "src")
+  # Get just the ID query
+  if (length(img) >= 1 && img[1] != "images/nophoto.gif"){
+    img <-  gsub(".*[?&]id=(\\d+)", "\\1", img[[1]], perl=TRUE)
+    pic <- GET("http://acm.co.lake.ca.us/sheriff/getimage.asp", query=list(id=img))
+    bin <- content(pic, "raw")
+    writeBin(bin, getImagePath(bookingID))
+  }
+  
   tabs <- readHTMLTable(html)
   
-  info <- list()
-  charges <- list()
   #Parse the individual tables in this document. They typically have the first row as
   # a section heder, then have alternating rows of titles and content which we'll want
   # to pair up in a list. The first one is usually empty, so start with the second.
-  for (t in 2:(length(tabs))){
-    thisTab <- processTable(bookingID, tabs[[t]], firstTable=(t==2), lastTable=(t==length(tabs)))
-    info <- c(info, thisTab$data)
-    charges <- c(charges, thisTab$charges)
-  }  
+  nameTrack <- processNameTrackingTable(tabs[[3]])
+  phys <- processTable(NULL, tabs[[4]], header=FALSE)$data
+  personal <- processTable(NULL, tabs[[5]], header=FALSE)$data
+  arrest <- processTable(NULL, tabs[[6]], header=FALSE)$data
+  courtChrg <- processTable(bookingID, tabs[[7]], header=FALSE, lastTable=TRUE)
+  court <- courtChrg$data
+  charges <- courtChrg$charges
+  
+  info <- list(NameTracking=nameTrack, Physical=phys, Personal=personal, Arrest=arrest,
+               Court=court)
+  
   return(list(data=info, charges=charges))
+}
+
+processNameTrackingTable <- function(table){
+  # Trim extra links off the bottom
+  table <- table[1:2,]
+  processTable(NULL, table, header=FALSE)$data
 }
 
 #' Parse a table having a content header row then alternating rows of 
@@ -50,12 +80,15 @@ downloadBooking <- function(bookingID){
 #' @param lastTable Whether or not this is the last table in the document. Currently, the
 #' last table requires extra formatting as it's actually two separate tables and some
 #' additional content at the end.
-processTable <- function(bookingID, table, firstTable=FALSE, lastTable=FALSE){
+processTable <- function(bookingID, table, header=TRUE, lastTable=FALSE){
+  firstTable <- FALSE
+  
+  
   if (!lastTable){
     if (firstTable){
       #trim to only second and third rows.
       table <- table[2:3,]
-    } else{    
+    } else if (header){    
       table <- table[-1,]
     }
     
@@ -105,22 +138,24 @@ processTable <- function(bookingID, table, firstTable=FALSE, lastTable=FALSE){
     results <- theseResults
     
     #Process all charges
-    labels <- as.character(unlist(table[3,]))
+    labels <- as.character(unlist(table[4,]))
     #remove trailing colons
     labels <- sub("(.*):", "\\1", labels)
+    omit <- is.na(labels)
+    labels <- labels[!omit]
     
     charges <- list()
     
-    for (i in 4:nrow(table)){
+    for (i in 5:nrow(table)){
       values <- as.character(unlist(table[i,]))
       #Sometimes included in extraneous spacing
       values <- gsub("Â", "", values)
+      values <- values[!omit]
       
       theseResults <- as.list(c(bookingID, values))
       names(theseResults) <- c("BookingID", labels)
             
       #Null holder in these tables is this character. If you see, ignore field.
-      theseResults <- theseResults[!is.na(names(theseResults))]
       theseResults <- theseResults[names(theseResults) != "Â"]      
       
       charges[[length(charges)+1]] <- theseResults
